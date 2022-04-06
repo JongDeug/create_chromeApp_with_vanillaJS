649dd23728aa6d54dc3395975fa2df4c7f99e1ba
JongDeug <pinpoint1998@gmail.com>
2022-04-06 18:14:08 +0900

    4.6

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-function plus(a, b){
-    console.log(a + b);
+const age = parseInt(prompt("How old are you?"));
+
+
+if(isNaN(age)){
+    prompt("you have to input number ok? fucking crazy man?");
 }
-function divide(a, b){
-    console.log(a / b);
+else if(age < 18 && age < 50){
+    console.log("You are too young.");
+}
+else{
+    console.log("You can't")
 }
 
-plus(50, 3);
-divide(98, 20);
-
-const player = {
-    name: "jong",
-    sayHello: function(name){
-        console.log("hello! my name is " + name);
-    }
-};
-console.log("");
-player.sayHello("jong");
