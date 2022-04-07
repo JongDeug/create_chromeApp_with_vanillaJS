d8f2fa885e1190ce53a40f5d2b26a7762cd68da3
JongDeug <pinpoint1998@gmail.com>
2022-04-07 18:49:15 +0900

    4.7

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1:first-child");
+// let flag = 0;
+// function handleTitleClick(){
+    
+//     if(flag == 0){
+//         title.style.color = "blue";
+//         flag = 1;
+//     } else if(flag == 1){
+//         title.style.color = "black";
+//         flag = 0;
+//     }
+// }
+// title.addEventListener("click", handleTitleClick);
+
+console.log(title.className);
+function handleMouseEnter(){
+    title.innerHTML = "mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerHTML = "mouse is gone";
+}
+
+function handleMouseDown(){
+    title.innerHTML = "mouse is down";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+title.onmousedown = handleMouseDown;
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+title.addEventListener("click", function(){
+    const clickClass = "active";
+
+    // if(title.classList.contains(clickClass)){
+    //     title.classList.remove(clickClass);
+    // }else {
+    //     title.classList.add(clickClass);
+    // }
+    // console.log(title.className);
+    title.classList.toggle("active");
+})
 
 
-if(isNaN(age)){
-    prompt("you have to input number ok? fucking crazy man?");
-}
-else if(age < 18 && age < 50){
-    console.log("You are too young.");
-}
-else{
-    console.log("You can't")
-}
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", function(){
+    alert("you copy is detected!")
+})
+window.addEventListener("offline", function(){
+    this.alert("SOS no WIFI");
+})
+window.addEventListener("online", function(){
+    this.alert("WIFI is connected");
+})
 
+
+console.dir(title);
